30d5f2941e0f4b1ca3723a805f0f57899bdfcf65
Leidy Torres <mile-97_torres@hotmail.com>
2019-08-18 10:26:45 -0500

    Update Secuencia.java
    Se agrego el constructor de la clase

--- src/main/java/Secuencia.java
+++ src/main/java/Secuencia.java
@@ -2,15 +2,22 @@
 import java.util.Scanner;
 
 
-/**
+ /**
  *Esta clase maneja la secuencia de numeros.
  * @author Duvan Poveda - Leidy Torres
  */
 public class Secuencia {
     /**
- *Variable para el manejo de la entrada del teclado
- */
+    *Variable para el manejo de la entrada del teclado
+    */
     private Scanner scanner = new Scanner(System.in);
+    
+    /**
+     * Constructor de la clase 
+     */
+    public Secuencia(){
+    
+    }
     
     /**
     *Este metodo se encarga de recibir los numeros que el usuario ingresa
@@ -34,7 +41,7 @@ public class Secuencia {
         ordenBurbuja(numero,contador);
     }
     
-       /**
+    /**
     *Este metodo se encarga de ordenar por el metodo de burbuja el vector recibido
     * @param numero
     * @param contador
@@ -63,38 +70,38 @@ public class Secuencia {
     * @param menor
     */
     public void mostrarFaltantes(int[] numero,int contador,int mayor,int menor){
-         int[] numerosRequeridos = new int[100];
-    int tamaño = mayor - menor;
-    for(int i=0;i<=tamaño; i++){
-        numerosRequeridos[i]=menor;
-        menor = menor+1;
-    }
-    int[] numerosFaltan = new int[10];
-    int error = 1;
-    int contadorFaltantes = 0;
-    for(int i=0;i<=tamaño; i++){
-        error = 1;
-        for(int j=0; j<=contador;j++){
-            if(numerosRequeridos[i]==numero[j]){
-                error = 0;
-                j=contador;
-            }
+        int[] numerosRequeridos = new int[100];
+        int tamaño = mayor - menor;
+        for(int i=0;i<=tamaño; i++){
+            numerosRequeridos[i]=menor;
+            menor = menor+1;
         }
-        if(error == 1){
-            for(int k=0;k<=tamaño;k++){
-                if(numerosFaltan[k]==0){
-                    numerosFaltan[k]=numerosRequeridos[i];
-                    contadorFaltantes = contadorFaltantes + 1;
-                    k= tamaño;
-                   
+        int[] numerosFaltan = new int[10];
+        int error = 1;
+        int contadorFaltantes = 0;
+        for(int i=0;i<=tamaño; i++){
+            error = 1;
+            for(int j=0; j<=contador;j++){
+                if(numerosRequeridos[i]==numero[j]){
+                    error = 0;
+                    j=contador;
+                }
+            }
+            if(error == 1){
+                for(int k=0;k<=tamaño;k++){
+                    if(numerosFaltan[k]==0){
+                        numerosFaltan[k]=numerosRequeridos[i];
+                        contadorFaltantes = contadorFaltantes + 1;
+                        k= tamaño;
+
+                    }
                 }
             }
         }
-    }
-    System.out.println("Los numeros Faltantes son:");
-    for(int i=0; i<=contadorFaltantes-1;i++){
-        
-        System.out.println(numerosFaltan[i]);
-    }
-    }
+        System.out.println("Los numeros Faltantes son:");
+        for(int i=0; i<=contadorFaltantes-1;i++){
+
+            System.out.println(numerosFaltan[i]);
+        }
+     }
 }
